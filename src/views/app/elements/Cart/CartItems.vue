9c6219dc7f316c0e5aee66cae088ad2bd4322461
Ivan Miljanić <imiljanic@agrokor.local>
2020-05-31 16:41:28 +0200

    added loaders

--- src/views/app/elements/Cart/CartItems.vue
+++ src/views/app/elements/Cart/CartItems.vue
@@ -3,7 +3,9 @@
     <div class="empty-space"></div>
     <v-row>
       <v-col cols="12" md="1" lg="1">
-        <v-btn color="red" class="white--text" rounded @click="goBack()"><v-icon left >mdi-arrow-left</v-icon> Back </v-btn>
+        <v-btn color="red" class="white--text" rounded @click="goBack()">
+          <v-icon left>mdi-arrow-left</v-icon>Back
+        </v-btn>
       </v-col>
       <v-col cols="12" md="10" lg="10">
         <v-data-table
@@ -13,7 +15,7 @@
           sort-by="name"
           class="elevation-1"
         >
-        <template v-slot:item.photo="{ item }">
+          <template v-slot:item.photo="{ item }">
             <div class="p-2">
               <v-img :src="item.photo" :alt="item.name" width="70px"></v-img>
             </div>
@@ -24,7 +26,10 @@
               <v-divider class="mx-4" inset vertical></v-divider>
               <v-spacer></v-spacer>
 
-              <v-btn color="error" @click="emptyCart()" class="mb-2" >Empty cart  <v-icon right>mdi-delete</v-icon></v-btn>
+              <v-btn color="error" @click="emptyCart()" class="mb-2">
+                Empty cart
+                <v-icon right>mdi-delete</v-icon>
+              </v-btn>
             </v-toolbar>
           </template>
           <template v-slot:item.actions="{ item }">
@@ -36,7 +41,10 @@
         </v-data-table>
       </v-col>
       <v-col cols="12" md="1" lg="1">
-        <v-btn color="red" class="white--text" rounded @click="goCheckout()">Checkout <v-icon right >mdi-check</v-icon></v-btn>
+        <v-btn color="red" class="white--text" rounded @click="goCheckout()">
+          Checkout
+          <v-icon right>mdi-check</v-icon>
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -124,16 +132,25 @@ export default {
     },
     emptyCart() {
       this.openNotification("bottom-center", "#B71C1C");
-      this.$store.commit("emptyCart", true)
-      this.initialize()
+      
     },
     openNotification(position = null, color) {
       this.$vs.notification({
+        loading: true,
+        duration: 2000,
+        color,
+        position,
+      });
+      setTimeout(() => (this.$store.commit("emptyCart", true),
+      this.initialize()), 2000)
+      setTimeout(() => (this.$vs.notification({
+
         color,
         position,
         title: "Cart",
         text: "Your cart is empty!"
-      });
+      })), 2500);
+      
     }
   }
 };
